9cc6f49b04206727bf852c72779310fa86de006b
yahelmm12-bit <yahelmm12@gmail.com>
2026-09-19 20:16:48 -0600

    Create break.py

---
+++ learning_documentation/02_while_loops/break.py
@@ -0,0 +1,7 @@
+#A code that sets a variable called "number" and set its value to 1. Then it enters a while loops until it reaches seven and stops the code completely.
+number = 1
+while True:
+    print(number)
+    if number == 7:
+        break
+    number += 1
