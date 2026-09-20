df66be224526cbcd614cd3c172e6c394f67262f4
yahelmm12-bit <yahelmm12@gmail.com>
2026-09-19 20:02:15 -0600

    Create even_numbers.py

---
+++ learning_documentation/02_while_loops/even_numbers.py
@@ -0,0 +1,6 @@
+#A simple program that uses while loops. As long as  the number is less than or equal to 20 and if the number is even it will be printed.
+number = 1
+while number <= 20:
+    if number % 2 == 0:
+        print(number)
+    number += 1
