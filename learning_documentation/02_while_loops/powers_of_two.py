0a7e8e061d00e8ee98efbf63860328f24f0d5bf1
yahelmm12-bit <yahelmm12@gmail.com>
2026-09-19 19:42:31 -0600

    Create powers_of_two.py
    Implement a program to print powers of two up to 100.

---
+++ learning_documentation/02_while_loops/powers_of_two.py
@@ -0,0 +1,5 @@
+#A simple program that prints number, starting with one, and keeps multiplying by 2 while number is less or equal to 100
+number = 1
+while number <= 100:
+    print(number)
+    number *= 2
