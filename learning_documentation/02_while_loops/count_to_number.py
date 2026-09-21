8ecab551743f1dbea70cc87998be140648031889
Yahel <yahelmm12@gmail.com>
2026-09-20 19:50:53 -0600

    Add counting script up to user-defined number
    This script prompts the user for a number and counts from 1 to that number, printing each value.

---
+++ learning_documentation/02_while_loops/count_to_number.py
@@ -0,0 +1,7 @@
+#A simple code that counts from one up to the input number.
+number = int(input("Hi, please input a number: "))
+counter = 1
+
+while counter <= number:
+    print(counter)
+    counter += 1
