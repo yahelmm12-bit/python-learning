be42c1a5e7bda35b14b0aa58bac10c4968aaffcd
Yahel <yahelmm12@gmail.com>
2026-09-20 19:53:17 -0600

    Add countdown program from user input
    Implement a countdown program that counts down from a user-defined number to one.

---
+++ learning_documentation/02_while_loops/countdown_input.py
@@ -0,0 +1,7 @@
+#A program that creates a countdown from the input number down to one.
+number = int(input("Enter a starting number: "))
+
+while number >= 1:
+    print(number)
+    number -= 1
+  
